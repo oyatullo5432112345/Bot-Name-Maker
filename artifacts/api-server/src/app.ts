8ac21627f6f615c08a807fe0f4d2c86df9b1e4a1
oyatullo5432112345 <oyatullo5432112345@gmail.com>
2026-08-12 13:04:43 +0500

    Add files via upload

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -37,8 +37,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
 app.use("/api", router);
 
